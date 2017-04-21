1c5c40a3e5712f8c2a8bfa18fe89d55bc805caa4
Nathan Saunders <nathan_saunders@hotmail.co.uk>
2017-04-21 10:02:18 +0000

    Updated flash message styles and js

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -9,7 +9,7 @@ class ProfilesController < ApplicationController
   # POST to /users/:user_id/profile
   def create
     # Ensure that we have the user who is filling out form
-    @user = User.find( params[:user_id])
+    @user = User.find( params[:user_id] )
     # Create profile linked to this specific user
     @profile = @user.build_profile( profile_params )
     if @profile.save
@@ -21,8 +21,19 @@ class ProfilesController < ApplicationController
   end
   
   def edit
-    @user = User.find( params[:user_id])
+    @user = User.find( params[:user_id] )
     @profile = @user.profile
+  end
+  
+  def update
+    @user = User.find( params[:user_id] )
+    @profile = @user.profile
+    if @profile.update_attributes(profile_params)
+      flash[:success] = "Profile updated!"
+      redirect_to user_path(id: params[:user_id] )
+    else
+      render action: :edit
+    end
   end
   
 private 
